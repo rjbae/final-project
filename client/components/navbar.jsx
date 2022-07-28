388f1b03fa1ba086daa1f9dc1daca9953bd9b710
Ryan Bae <baeryan21@gmail.com>
2022-07-28 17:35:33 +0000

    Deleted bootstrap imports from navbar component.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 
 export default function NavBar() {
