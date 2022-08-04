f9cac1402c9cafaf6ace9a5a13274bc7576dbd86
Ryan Bae <baeryan21@gmail.com>
2022-08-04 07:37:19 +0000

    Completed feature where user can see their posts on their profile.

--- client/pages/my-profile.jsx
+++ client/pages/my-profile.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 
 export default class MyProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClicked: false
+      isClicked: false,
+      posts: []
     };
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    const req = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Token': localStorage.getItem('react-context-jwt')
+      }
+    };
+    fetch('/api/posts', req)
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }));
   }
 
   handleClick() {
@@ -20,14 +33,21 @@ export default class MyProfile extends React.Component {
 
   render() {
     return (
-      <Container>
       <>
-        <div className='d-flex justify-content-between'>
-            <h2 className='profile-header'>My Profile</h2>
-            <h2 className='post-header'>Post <a href='#create-post'><i className="fas fa-plus"></i></a></h2>
-        </div>
+      <div className='d-flex justify-content-between'>
+        <h2 className='profile-header'>My Profile</h2>
+        <h2 className='post-header'>Post <a href='#create-post'><i className="fas fa-plus"></i></a></h2>
+      </div>
+      <div className='profile-container justify-content-evenly align-items-center'>
+            {this.state.posts.map(post =>
+            <div key={post.userId} className='row'>
+              <div className='col-sm-3'>
+                <img src={post.photoUrl} className='profile-posts'></img>
+              </div>
+            </div>
+            )}
+      </div>
       </>
-      </Container>
     );
   }
 }
